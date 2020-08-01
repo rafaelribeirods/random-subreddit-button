ef4db9e3c81aca800b966919c5b1bb86abc345d4
Rafael Ribeiro <rafaelribeiros1997@gmail.com>
2020-08-01 16:59:23 -0300

    Adicionando botão mesmo quando o usuário não está logado

--- scripts/rsb.js
+++ scripts/rsb.js
@@ -1,6 +1,20 @@
 window.onload=function() { 
 
-    const bar = document.getElementById('change-username-tooltip-id');
+    let bar = document.getElementById('change-username-tooltip-id');
+
+    if(bar.children.length > 0) {
+        authenticated(bar);
+    }
+    else {
+        bar = document.getElementsByClassName('header-user-dropdown')[0];
+        notAuthenticated(bar);
+    }
+    
+
+}
+
+function authenticated(bar) {
+
     const button_example = bar.children[0];
     const image_size = button_example.children[0].children[0].clientHeight;
     
@@ -16,4 +30,26 @@ window.onload=function() {
     button.children[0].appendChild(img);
     bar.appendChild(button);
 
+}
+
+function notAuthenticated(bar) {
+
+    const button_example = bar.children[0];
+    const image_size = button_example.children[0].children[0].children[0].clientHeight;
+    const margin = window.getComputedStyle(button_example).getPropertyValue("margin-left");
+
+    let img = document.createElement('img');
+    img.src = chrome.runtime.getURL("images/icon.png");
+    img.style.height = image_size + 'px';
+    img.style.width = image_size + 'px';
+
+    let container = document.createElement('div');
+    container.style.marginLeft = margin;
+    let link = document.createElement('a');
+    link.setAttribute('href', "https://reddit.com/r/random");
+    link.appendChild(img);
+    container.appendChild(link);
+
+    document.getElementById('email-verification-tooltip-id').prepend(container);
+    
 }
